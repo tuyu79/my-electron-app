de46c6250f15c0804e5a38aac818f96572968214
abel <593051107@qq.com>
2024-11-14 14:35:37 +0800

    init

---
+++ preload.js
@@ -0,0 +1,19 @@
+const { log } = require("console");
+const fs = require("fs");
+// preload.js
+
+// 所有的 Node.js API接口 都可以在 preload 进程中被调用.
+// 它拥有与Chrome扩展一样的沙盒。
+window.addEventListener("DOMContentLoaded", () => {
+  const buffer = fs.readFileSync('/Users/abel/Desktop/my-electron-app/main.js');
+  log(buffer)
+
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector);
+    if (element) element.innerText = text;
+  };
+
+  for (const dependency of ["chrome", "node", "electron"]) {
+    replaceText(`${dependency}-version`, process.versions[dependency]);
+  }
+});
